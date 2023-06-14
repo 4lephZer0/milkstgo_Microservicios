a4c8206e648dbf13f04a7b8088064b61e473774b
Alan Donoso <Alan.Donoso.V@usach.cl>
2023-06-14 17:55:03 -0400

    primer commit

---
+++ Backend/Grasas/grasas/src/main/java/com/milkstgo/grasas/GrasasApplication.java
@@ -0,0 +1,13 @@
+package com.milkstgo.grasas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrasasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrasasApplication.class, args);
+	}
+
+}
---
+++ Backend/PlanillaPago/planillaPago/src/main/java/com/milkstgo/planillaPago/PlanillaPagoApplication.java
@@ -0,0 +1,13 @@
+package com.milkstgo.planillaPago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanillaPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanillaPagoApplication.class, args);
+	}
+
+}
